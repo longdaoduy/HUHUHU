96cc0d1343d87f12b63929af5db36488bf488f7a
24127152 <nvtdat2435@clc.fitus.edu.vn>
2025-10-15 17:19:25 +0700

    feat: Add .gitignore and initial bai4.1.py

---
+++ bai4.1.py
@@ -0,0 +1,8 @@
+def leapYear(n):
+
+    if n % 400 == 0 or (n % 4 == 0 and n % 100 != 0):
+        return True
+    return False
+
+n = int(input("Input Year: "))
+print(leapYear(n))
---
+++ bai4.8.py
@@ -0,0 +1,6 @@
+def splitWord(s):
+    #phải có ngoặc tròn vì là tên hàm
+    return s.split()
+
+text = str(input("Input text: "))
+print(splitWord(text))
