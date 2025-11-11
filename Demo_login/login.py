6967bbdd6aa0c0b7e68f6fcb124d98fbb429b1af
24127152 <nvtdat2435@clc.fitus.edu.vn>
2025-11-11 11:44:16 +0700

    Update login.py

--- Demo_login/login.py
+++ Demo_login/login.py
@@ -1,7 +1,77 @@
 import streamlit as st
 import streamlit_authenticator as stauth
+from datetime import datetime
+import base64
 import json
 import os
+ALBUM_FILE = "Users_album.json"
+def loadUserAlbum(username):
+    if not os.path.exists(ALBUM_FILE):
+        return {}
+    with open(ALBUM_FILE, 'r',encoding='utf-8') as f:
+        try:
+            all_album = json.load(f)
+        except json.JSONDecodeError:
+            return {}
+    #Lấy album của người
+    user_album = all_album.get(username ,{})
+    loaded_albums = {}
+    for album_name, items in user_album.items():
+        loaded_albums[album_name] = []
+        for item in items:
+            #Tạo bản sao item
+            new_item = item.copy()
+            if 'bytes' in new_item and isinstance(new_item['bytes'], str):
+                try:
+                    # Giải mã chuỗi Base64 trở lại thành dữ liệu bytes
+                    decoded_bytes = base64.b64decode(new_item['bytes'])
+                    new_item['bytes'] = decoded_bytes
+                except:
+                    # Nếu giải mã lỗi, gán None để báo lỗi dữ liệu
+                    new_item['bytes'] = None
+            loaded_albums[album_name].append(new_item)
+    
+    return loaded_albums
+
+def saveUserAlbums(username, user_albums):
+    """
+    Lưu dữ liệu album của người dùng vào file JSON.
+    Mã hóa bytes ảnh thành chuỗi Base64.
+    """
+    
+    # Tải tất cả dữ liệu hiện có để tránh ghi đè người dùng khác
+    if os.path.exists(ALBUM_FILE):
+        with open(ALBUM_FILE, 'r', encoding='utf-8') as f:
+            try:
+                all_albums_data = json.load(f)
+            except json.JSONDecodeError:
+                all_albums_data = {}
+    else:
+        all_albums_data = {}
+
+    #Mã hóa (Base64) dữ liệu của người dùng hiện tại
+    albums_to_save = {}
+    for album_name, items in user_albums.items():
+        albums_to_save[album_name] = []
+    
+        for item in items:
+            # Tạo bản sao để không làm thay đổi item trong session_state
+            new_item = item.copy() 
+            
+            # Mã hóa bytes ảnh sang chuỗi Base64 để lưu vào JSON
+            if 'bytes' in new_item and isinstance(new_item['bytes'], bytes):
+                encoded_bytes = base64.b64encode(new_item['bytes']).decode('utf-8')
+                new_item['bytes'] = encoded_bytes
+    
+            albums_to_save[album_name].append(new_item)
+
+    # Cập nhật và ghi lại toàn bộ file
+    all_albums_data[username] = albums_to_save
+    
+    with open(ALBUM_FILE, 'w', encoding='utf-8') as f:
+        json.dump(all_albums_data, f, indent=4, ensure_ascii=False)
+
+
 def loadUser():
     if not os.path.exists("Users.json"):
         return {"users" : []}
@@ -71,4 +141,6 @@ def screen_login_page(data):
 
     
     
+            
+
             
